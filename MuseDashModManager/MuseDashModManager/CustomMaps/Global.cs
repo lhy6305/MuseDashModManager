d171cff4ce19742a11e860f567265e5687206bea
lhy6305 <78890774+lhy6305@users.noreply.github.com>
2021-02-21 16:14:01 +0800

    Add files via upload

---
+++ MuseDashModManager/MuseDashModManager/CustomMaps/Global.cs
@@ -0,0 +1,28 @@
+﻿using GameLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MuseDashModManager.CustomMaps
+{
+    class Global
+    {
+        public static MusicConfigReader MusicConfigReader = (MusicConfigReader)Activator.CreateInstance(typeof(MusicConfigReader), true);
+
+        public static Sprite SpriteNoCover = null;
+
+        public static CustomMapNameListVariable CustomMapNameListVariable = new CustomMapNameListVariable();
+
+        public static CustomImageVariable ImageVariable = new CustomImageVariable();
+
+        public static string MapDirectory = "custom_maps";
+		
+		public static string AssetsDirectory = "custom_assets";
+
+        public static List<CustomMap> CustomMapList = new List<CustomMap>();
+
+        public static GameObject CustomAlbumCells;
+    }
+}
---
+++ MuseDashModManager/MuseDashModManager/Patch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Harmony;
+
+namespace MuseDashModManager
+{
+    class Patch
+    {
+        private static HarmonyInstance harmonyInstance = HarmonyInstance.Create("pw.baka.musedash.modmanager");
+
+        public static void InstallPatches()
+        {
+            // CustomMap
+            MuseDashModManager.CustomMaps.Patch.InstallPatch(harmonyInstance);
+            
+        }
+    }
+}
